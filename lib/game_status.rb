67a630612f12e9bb79e2fa38415ca2b8454b70bc
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-14 12:34:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,11 +31,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else
-    return false
-  end
+  !won?(board) && full?(board)
 end
 
 def over?(board)
